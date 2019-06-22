95508e205d8cd9511af4dfcfb77f4accb3b9bdb5
Guilherme Henrique <guilherme.pomp@hotmail.com>
2019-06-22 14:09:59 -0300

    Add project files.

---
+++ src/CI.Infrastructure/Data/ControleInvestimentosContext.cs
@@ -0,0 +1,23 @@
+﻿using CI.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CI.Infrastructure.Data
+{
+    public class ControleInvestimentosContext : DbContext
+    {
+        public ControleInvestimentosContext(DbContextOptions<ControleInvestimentosContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pessoa>().ToTable("Pessoa");
+        }
+    }
+}
