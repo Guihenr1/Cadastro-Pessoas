8f5f0aff50e685acc89ff3b90e95199a5f0d520d
Guilherme Henrique <guilherme.pomp@hotmail.com>
2019-06-29 17:29:57 -0300

    Adiciona Repository em Infrastructure.

---
+++ src/CI.Infrastructure/Repository/EFRepository.cs
@@ -0,0 +1,55 @@
+﻿using CI.Domain.Interfaces.Repository;
+using CI.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CI.Infrastructure.Repository
+{
+    public class EFRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly ControleInvestimentosContext _dbContext;
+
+        public EFRepository(ControleInvestimentosContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public TEntity Adicionar(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Add(entity);
+            _dbContext.SaveChanges();
+            return entity;
+        }
+
+        public void Atualizar(TEntity entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicado)
+        {
+            return _dbContext.Set<TEntity>().Where(predicado).AsEnumerable();
+        }
+
+        public TEntity ObterPorId(int id)
+        {
+            return _dbContext.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> ObterTodos()
+        {
+            return _dbContext.Set<TEntity>().AsEnumerable();
+        }
+
+        public void Remover(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+    }
+}
